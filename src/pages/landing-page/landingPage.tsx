88dd4032d1d42046742807ec48f2af6d4878f34d
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2022-12-24 22:22:28 +0800

    feat(Landing): Include description text

--- src/pages/landing-page/landingPage.tsx
+++ src/pages/landing-page/landingPage.tsx
@@ -5,15 +5,29 @@ interface LandingPageProps {
   isMobile: boolean;
 }
 
+interface HighlightTextProps {
+  children: React.ReactNode;
+}
+
+const HighlightText = ({ children }: HighlightTextProps) => <span className="text-[#438EFF]">{children}</span>;
+const HorizontalLine: React.FC<{className: string}> = ({ className }) => <div className={`${className} border-t border-[#438EFF]`}/>
+
 const LandingPage = ({ className, isMobile }: LandingPageProps) => {
   return <div id="landing" className={`${className} bg-[#000000] text-[#FFFFFF] p-5 flex ${ isMobile ? "flex-col-reverse" : "flex-row" } gap-2 items-center justify-center`}>
-    <div className="flex flex-col items-end">
-      <p className="text-right text-[#595959] text-[40px]">Hello there! I am</p>
-      <div className="w-fit flex flex-col items-end gap-1">
-        <p className="text-right text-[#FFFFFF] text-[40px]">Goh Jun Yi</p>
-        <div className="border-t w-full border-[#438EFF]"/>
-        <div className="border-t w-[60%] border-[#438EFF]"/>
+    <div className="flex flex-col items-end gap-4">
+      <div className="flex flex-col items-end w-fit">
+        <p className="text-right text-[#595959] text-[40px] leading-[50px]">Hello there! I am</p>
+
+        <div className="w-fit flex flex-col items-end gap-1">
+          <p className="text-right text-[#FFFFFF] text-[40px] leading-[50px]">Goh Jun Yi</p>
+          <HorizontalLine className="w-full"/>
+          <HorizontalLine className="w-[60%]"/>
+        </div>
       </div>
+
+      <p className="w-[60%] break-words text-right text-[#595959]">
+        I am currently a <HighlightText>Year 2</HighlightText> computer science student in <HighlightText>National University of Singapore</HighlightText>.
+      </p>
     </div>
 
     <img src={BackgroundImg} width={isMobile ? "100%" : "40%"}/>
