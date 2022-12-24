368eda623e9c29e4d933509cc2e4223724340b5d
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2022-12-24 21:49:17 +0800

    feat(Landing): implement menu icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from 'react';
 import LandingPage from './pages/landing-page/landingPage';
 import ExperiencesPage from './pages/experiences-page/experiencesPage';
 
+import {ReactComponent as ResumeIcon} from './assets/resume_icon.svg';
+import {ReactComponent as GithubIcon} from './assets/github_icon.svg';
+import {ReactComponent as LinkedinIcon} from './assets/linkedin_icon.svg';
+import {ReactComponent as EmailIcon} from './assets/email_icon.svg';
+
 function App() {
   const [windowWidth, setWindowWidth] = useState<number>(0);
   const [isMobile, setIsMobile] = useState<boolean>(false);
@@ -19,24 +24,33 @@ function App() {
   useEffect(() => setIsMobile(windowWidth < MOBILE_WIDTH), [windowWidth]);
 
   const VerticalLine = () => <div className="h-[5%] w-0 border-l border-[#FFFFFF] gap-1"/>;
+  const QuickAccessIcons = () => <>
+    <ResumeIcon />
+    <GithubIcon />
+    <LinkedinIcon />
+    <EmailIcon />
+  </>;
 
   const ContentPageCSS = `relative ${ windowWidth > 600 ? "left-[5%] w-[85%]" : "left-0 w-full" } h-screen snap-start`;
 
   return (
     <div className="h-screen w-full overflow-y-auto scroll-smooth snap-y snap-mandatory bg-[#000000]">
-      { windowWidth > 600 && 
-        <div className="absolute left-0 w-[5%] h-screen flex flex-col gap-1 justify-start items-center">
+      { !isMobile && 
+        <div className="absolute left-0 w-[5%] h-screen flex flex-col gap-5 justify-start items-center">
           <VerticalLine />
+          <QuickAccessIcons />
         </div>
       }
 
       <LandingPage className={ContentPageCSS} isMobile={isMobile}/>
       <ExperiencesPage className={ContentPageCSS} isMobile={isMobile}/>
       
-      { windowWidth > 600 && 
-        <div className="absolute top-0 right-0 w-[10%] h-screen flex flex-col gap-1 justify-end items-center">
+      { !isMobile &&
+        <div className="absolute top-0 right-0 w-[10%] h-screen flex flex-col gap-2 justify-end items-end pr-10 text-[#FFFFFF]">
+          <a href="#landing">Welcome</a>
+          <a href="#experiences">Experiences</a>
           <VerticalLine />
-        </div>
+        </div> 
       }
     </div>
   );
--- src/pages/landing-page/landingPage.tsx
+++ src/pages/landing-page/landingPage.tsx
@@ -6,8 +6,8 @@ interface LandingPageProps {
 }
 
 const LandingPage = ({ className, isMobile }: LandingPageProps) => {
-  return <div className={`${className} bg-[#000000] text-[#FFFFFF] p-5 flex ${ isMobile ? "flex-col-reverse" : "flex-row" } gap-2 items-center justify-center`}>
-    <div className="flex flex-col items-start">
+  return <div id="landing" className={`${className} bg-[#000000] text-[#FFFFFF] p-5 flex ${ isMobile ? "flex-col-reverse" : "flex-row" } gap-2 items-center justify-center`}>
+    <div className="flex flex-col items-end">
       <p className="text-right text-[#595959] text-[40px]">Hello there! I am</p>
       <div className="w-fit flex flex-col items-end gap-1">
         <p className="text-right text-[#FFFFFF] text-[40px]">Goh Jun Yi</p>
