9541eb35ee8cf23a885172a59a135724e12c9c7d
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2022-12-28 14:43:23 +0800

    feat: Introduce more logos for usage

--- src/components/logo-icon/logoIcon.tsx
+++ src/components/logo-icon/logoIcon.tsx
@@ -2,13 +2,31 @@ import {ReactComponent as ReactIcon} from "assets/icons/react.svg";
 import {ReactComponent as IonicIcon} from "assets/icons/ionic.svg";
 import {ReactComponent as ReduxIcon} from "assets/icons/redux.svg";
 import {ReactComponent as TypeScriptIcon} from "assets/icons/typescript.svg";
-import {ReactComponent as TailwindCSSIcon} from "assets/icons/tailwind-css.svg";
+import {ReactComponent as TailwindCSSIcon} from "assets/icons/tailwindcss-icon.svg";
 import {ReactComponent as PythonIcon} from "assets/icons/python.svg";
 import {ReactComponent as PyTorchIcon} from "assets/icons/pytorch-icon.svg";
 import {ReactComponent as GithubIcon} from "assets/icons/github.svg";
 import {ReactComponent as FigmaIcon} from "assets/icons/figma.svg";
 import {ReactComponent as DevPostIcon} from "assets/icons/devpost.svg";
 import {ReactComponent as OpenIcon} from "assets/icons/open-in-new.svg";
+import {ReactComponent as PostgreSQLIcon} from "assets/icons/postgresql.svg";
+import {ReactComponent as GitIcon} from "assets/icons/git.svg";
+import {ReactComponent as JavaIcon} from "assets/icons/java.svg";
+import {ReactComponent as RubyIcon} from "assets/icons/ruby.svg";
+import {ReactComponent as TensorFlowIcon} from "assets/icons/tensorflow-icon.svg";
+import {ReactComponent as StorybookIcon} from "assets/icons/storybook.svg";
+import {ReactComponent as CIcon} from "assets/icons/c.svg";
+import {ReactComponent as JavaScriptIcon} from "assets/icons/javascript.svg";
+import {ReactComponent as MaterialUIIcon} from "assets/icons/material-ui.svg";
+import {ReactComponent as RailsIcon} from "assets/icons/rails.svg";
+import {ReactComponent as DockerIcon} from "assets/icons/docker-tile.svg";
+import {ReactComponent as DashIcon} from "assets/icons/dash_plotly.svg";
+import {ReactComponent as FlaskIcon} from "assets/icons/flask-icon.svg";
+import {ReactComponent as DjangoIcon} from "assets/icons/django-logo-negative.svg";
+import {ReactComponent as MangoDBIcon} from "assets/icons/mongodb.svg";
+import {ReactComponent as Neo4JIcon} from "assets/icons/neo4j-icon-white.svg"; 
+import {ReactComponent as NginxIcon} from "assets/icons/nginx-icon.svg"; 
+import {ReactComponent as MySQLIcon} from "assets/icons/mysql-icon.svg"; 
 
 export enum LogoType {
   React = "React",
@@ -22,6 +40,24 @@ export enum LogoType {
   Figma = "Figma",
   DevPost = "DevPost",
   View = "View",
+  PostgreSQL = "PostgreSQL",
+  Git = "Git",
+  Java = "Java",
+  Ruby = "Ruby",
+  TensorFlow = "TensorFlow",
+  Storybook = "Storybook",
+  C = "C",
+  JavaScript = "JavaScript",
+  MaterialUI = "MaterialUI",
+  Rails = "Rails",
+  Docker = "Docker",
+  Dash = "Dash/Plotly",
+  Flask = "Flask",
+  Django = "Django",
+  MongoDB = "MongoDB",
+  Neo4J = "Neo4J",
+  Nginx = "Nginx",
+  MySQL = "MySQL",
 }
 
 const LogoSVG = {
@@ -36,6 +72,24 @@ const LogoSVG = {
   Figma: FigmaIcon,
   DevPost: DevPostIcon,
   View: OpenIcon,
+  PostgreSQL: PostgreSQLIcon,
+  Git: GitIcon,
+  Java: JavaIcon,
+  Ruby: RubyIcon,
+  TensorFlow: TensorFlowIcon,
+  Storybook: StorybookIcon,
+  C: CIcon,
+  JavaScript: JavaScriptIcon,
+  MaterialUI: MaterialUIIcon,
+  Rails: RailsIcon,
+  Docker: DockerIcon,
+  "Dash/Plotly": DashIcon,
+  Flask: FlaskIcon,
+  Django: DjangoIcon,
+  MongoDB: MangoDBIcon,
+  Neo4J: Neo4JIcon,
+  Nginx: NginxIcon,
+  MySQL: MySQLIcon,
 }
 
 const LogoLink = {
@@ -50,19 +104,39 @@ const LogoLink = {
   Figma: "https://www.figma.com/",
   DevPost: "https://devpost.com/",
   View: "",
+  PostgreSQL: "https://www.postgresql.org/",
+  Git: "https://git-scm.com/",
+  Java: "https://www.java.com/",
+  Ruby: "https://www.ruby-lang.org/",
+  TensorFlow: "https://www.tensorflow.org/",
+  Storybook: "https://storybook.js.org/",
+  C: "https://en.wikipedia.org/wiki/C_(programming_language)",
+  JavaScript: "https://www.javascript.com/",
+  MaterialUI: "https://mui.com/",
+  Rails: "https://rubyonrails.org/",
+  Docker: "https://www.docker.com/",
+  "Dash/Plotly": "https://dash.plotly.com/",
+  Flask: "https://flask.palletsprojects.com/",
+  Django: "https://www.djangoproject.com/",
+  MongoDB: "https://www.mongodb.com/",
+  Neo4J: "https://neo4j.com/",
+  Nginx: "https://www.nginx.com/",
+  MySQL: "https://www.mysql.com/",
 }
 
 interface LogoIconProps {
   name: LogoType;
+  length?: string;
   link?: string;
 }
 
-const LogoIcon = ({ name, link }: LogoIconProps) => {
+const LogoIcon = ({ name, length, link }: LogoIconProps) => {
+  const DEFAULT_LENGTH = "24px";
   const onLogoClick = () => window.open(link || LogoLink[name]);
 
   const Icon = LogoSVG[name];
   return <div className="flex flex-col items-center [&>*]:hover:opacity-70 [&>p]:hover:visible w-[32px]" onClick={onLogoClick}>
-    <Icon width="24px" height="24px" />
+    <Icon width={length || DEFAULT_LENGTH} height={length || DEFAULT_LENGTH} />
     <p className="text-[10px] invisible">{name}</p>
   </div>
 };
