18d831f034c1c788c8c6e993561aafed0d79463a
Goh Jun Yi <54541329+Junyi00@users.noreply.github.com>
2023-07-15 21:08:52 +0800

    feat: Update styles for project entry cards

--- src/pages/projects-page/projectEntry.tsx
+++ src/pages/projects-page/projectEntry.tsx
@@ -58,15 +58,15 @@ const ProjectEntry = ({
       >
         <img alt={name + "_image"} src={image} />
       </div>
-      <h3 className="md:hidden text-[20px] font-bold tracking-wider text-text col-span-3 transition-all ease-in-out">
+      <h3 className="md:hidden text-[20px] font-semibold tracking-wider text-text col-span-3 transition-all ease-in-out">
         {expanded ? name.toUpperCase() : shortName.toUpperCase()}
       </h3>
-      <h3 className="hidden md:block text-[20px] font-bold tracking-wider text-text col-span-3">
+      <h3 className="hidden md:block text-[20px] font-semibold text-text col-span-3">
         {shortName.toUpperCase()}
       </h3>
       <div className={`md:hidden w-full flex flex-col gap-2 h-fit max-h-0 overflow-hidden transition-all duration-500 ${expanded ? "max-h-[10000px] overflow-visible ease-in" : "ease-out"}`}>
         <div className="flex flex-col">
-          <p className="text-[15px] font-bold text-primary">{date}</p>
+          <p className="text-[15px] font-bold text-subtext">{date}</p>
         </div>
         
         {
